5bc0d7f542260317c3093b79134ba91a622f6061
dikayudha8 <andika@hopetechnik.com>
2017-11-23 14:13:06 +0800

    adding external interrupt of STM32F4 library

---
+++ ExternalInterrupt/ExternalInterrupt.h
@@ -0,0 +1,8 @@
+#include <cmsis_lib/include/stm32f4xx_exti.h>
+#include <cmsis_lib/include/stm32f4xx_gpio.h>
+#include <cmsis_lib/include/misc.h>
+#include <cmsis_lib/include/stm32f4xx_rcc.h>
+#include <cmsis_lib/include/stm32f4xx_syscfg.h>
+
+void ExternalInterruptInit();
+
